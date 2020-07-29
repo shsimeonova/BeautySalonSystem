f6a8d894fbbbad816e5aa7a0aa7d7db3c45b6b62
Simona Simeonova <simona.simeonova@hotmail.com>
2020-07-29 16:57:17 +0300

    Added message for no records in appointments and offers admin panel

--- BeautySalonSystem.UI/Pages/Admin/Appointments/Index.cshtml.cs
+++ BeautySalonSystem.UI/Pages/Admin/Appointments/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using AutoMapper;
 using BeautySalonSystem.UI.Models;
 using BeautySalonSystem.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BeautySalonSystem.UI.Pages.Admin.Appointments
@@ -24,21 +25,26 @@ namespace BeautySalonSystem.UI.Pages.Admin.Appointments
 
         public IEnumerable<AppointmentAdminViewModel> AppointmentRequests;
         
-        public void OnGet()
+        public IActionResult OnGet()
         {
             List<AppointmentViewModel> appointmentRequests = _appointmentsService.GetAllNonConfirmed().ToList();
             HashSet<int> offerIds = appointmentRequests.Select(a => a.offerId).ToHashSet();
-            Dictionary<int, OfferViewModel> offers = _offersService.GetManyByIds(offerIds.ToArray(), true);
-            appointmentRequests.ForEach(a => a.offer = offers[a.offerId]);
-            List<AppointmentAdminViewModel> result = new List<AppointmentAdminViewModel>();
-            foreach (var request in appointmentRequests)
+            if (appointmentRequests.Count > 0)
             {
-                AppointmentAdminViewModel viewModel = _mapper.Map<AppointmentViewModel, AppointmentAdminViewModel>(request);
-                viewModel.UserInfo = _userService.GetUserPersonalInfo(request.customerId);
-                result.Add(viewModel);
+                Dictionary<int, OfferViewModel> offers = _offersService.GetManyByIds(offerIds.ToArray(), true);
+                appointmentRequests.ForEach(a => a.offer = offers[a.offerId]);
+                List<AppointmentAdminViewModel> result = new List<AppointmentAdminViewModel>();
+                foreach (var request in appointmentRequests)
+                {
+                    AppointmentAdminViewModel viewModel = _mapper.Map<AppointmentViewModel, AppointmentAdminViewModel>(request);
+                    viewModel.UserInfo = _userService.GetUserPersonalInfo(request.customerId);
+                    result.Add(viewModel);
+                }
+
+                AppointmentRequests = result;
             }
 
-            AppointmentRequests = result;
+            return Page();
         }
 
         public void OnPostConfirmAppointmentRequest(int id)
